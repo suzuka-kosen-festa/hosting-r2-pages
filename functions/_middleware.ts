06eb355ade9e8f3ef22e45c0596d5f1b4bd19166
shiron <shiron4710.dev@gmail.com>
2024-05-29 11:22:30 +0900

    fix: something

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -5,8 +5,16 @@ interface Env {
 export const onRequest: PagesFunction<Env> = async (context) => {
   const { request } = context;
   const { url } = request;
-  const { pathname } = new URL(url);
+  let { pathname } = new URL(url);
+
+  if (pathname.endsWith("/")) {
+    pathname += "index.html";
+  }
+
   const obj = await context.env.BUCKET.get(pathname);
+  if (obj === null) {
+    return new Response("Not found", { status: 404 });
+  }
 
   return new Response(obj.body);
 };
