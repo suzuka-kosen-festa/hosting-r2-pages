08e3af46354e448e6b99e7d5032d073a2f0c338c
shiron <shiron4710.dev@gmail.com>
2024-05-29 11:17:50 +0900

    feat: bind r2

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,20 +1,12 @@
 interface Env {
-  KV: KVNamespace;
+  BUCKET: R2Bucket;
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-  const endpoint = await context.env.KV.get("endpoint");
-
   const { request } = context;
   const { url } = request;
   const { pathname } = new URL(url);
-  const newUrl = `${endpoint}${pathname}`;
-  const newRequest = new Request(newUrl, {
-    body: request.body,
-    headers: request.headers,
-    method: request.method,
-    redirect: request.redirect,
-  });
+  const obj = await context.env.BUCKET.get(pathname);
 
-  return fetch(newRequest);
+  return new Response(obj.body);
 };
